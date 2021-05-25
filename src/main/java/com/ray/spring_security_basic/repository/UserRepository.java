14c7e0073d2379de677cce667387e3d173d6d650
Ray <55176622+kdl9405@users.noreply.github.com>
2021-05-26 00:04:03 +0900

    #4 로그인

--- src/main/java/com/ray/spring_security_basic/repository/UserRepository.java
+++ src/main/java/com/ray/spring_security_basic/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
 
-   public User findbyUsername(String username);
+   public User findByUsername(String username);
 
-    
+   
 }
